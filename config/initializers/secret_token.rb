b429c2c134737cf0c64e11bd1f484e29d606527f
Ryan MacGillivray <rmacg89@gmail.com>
2013-12-08 22:04:40 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecretSanta::Application.config.secret_key_base = '3c83039e698d87d3ef28b281596f56014d5b00706bd8cfce0ed40b5d6a811ba722c64f769d1803d17c46f56a25e99b61d7370f8b1ac752427cf5ebd5db379590'
