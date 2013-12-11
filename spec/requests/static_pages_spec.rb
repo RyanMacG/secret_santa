14a0ff309de67dfc6119b348b29dbc9f245727fd
Ryan MacGillivray <rmacg89@gmail.com>
2013-12-11 22:17:27 +0000

    update rspec, add named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,21 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Secret Santa" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Secret Santa')
-    end
+    before { visit root_path }
+    it { should have_content('Home') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
+    it { should have_content('Help') }
   end
 
   describe "About page" do
-    it "should have the content 'About" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
+    before { visit about_path }
+    it { should have_content('Help') }
   end
 end
